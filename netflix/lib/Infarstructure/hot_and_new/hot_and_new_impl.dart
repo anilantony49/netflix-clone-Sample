5e05fc93941ccc65ea88ce993b247ebcf6fe6f9c
Anil Antony <anilvithayathil007@gmail.com>
2022-09-12 21:56:45 +0530

    everyones watching UI and api completed

--- netflix/lib/Infarstructure/hot_and_new/hot_and_new_impl.dart
+++ netflix/lib/Infarstructure/hot_and_new/hot_and_new_impl.dart
@@ -9,48 +9,47 @@ import 'package:netflix/Domain/hot_and_new_resp/models/hot_and_new_resp.dart';
 
 import '../../Domain/core/api_endpoints.dart';
 
-@LazySingleton(as:HotAndNewService)
-class HotAndNewImplementation implements HotAndNewService{ 
+@LazySingleton(as: HotAndNewService)
+class HotAndNewImplementation implements HotAndNewService {
   @override
-   Future<Either<MainFailure, HotAndNewResp>> getHotAndNewMovieData() async{
-  try {
-      final Response response = await Dio(BaseOptions())
-          .get(ApiEndPoint.hotandnewMovies,);
+  Future<Either<MainFailure, HotAndNewResp>> getHotAndNewMovieData() async {
+    try {
+      final Response response = await Dio(BaseOptions()).get(
+        ApiEndPoint.hotandnewMovies,
+      );
       if (response.statusCode == 200 || response.statusCode == 201) {
         final result = HotAndNewResp.fromJson(response.data);
         return Right(result);
       } else {
         return const Left(MainFailure.ServerFailure());
       }
-    }on DioError 
-     catch (e) {
+    } on DioError catch (e) {
       log(e.toString());
       return const Left(MainFailure.ClientFailure());
     } catch (e) {
-       log(e.toString());
-       return const Left(MainFailure.ClientFailure());
+      log(e.toString());
+      return const Left(MainFailure.ClientFailure());
     }
   }
 
   @override
-  Future<Either<MainFailure, HotAndNewResp>> getHotAndNewTvData() async{
+  Future<Either<MainFailure, HotAndNewResp>> getHotAndNewTvData() async {
     try {
-      final Response response = await Dio(BaseOptions())
-          .get(ApiEndPoint.hotandnewTv,);
+      final Response response = await Dio(BaseOptions()).get(
+        ApiEndPoint.hotandnewTv,
+      );
       if (response.statusCode == 200 || response.statusCode == 201) {
         final result = HotAndNewResp.fromJson(response.data);
         return Right(result);
       } else {
         return const Left(MainFailure.ServerFailure());
       }
-    }on DioError 
-     catch (e) {
+    } on DioError catch (e) {
       log(e.toString());
       return const Left(MainFailure.ClientFailure());
     } catch (e) {
-       log(e.toString());
-       return const Left(MainFailure.ClientFailure());
+      log(e.toString());
+      return const Left(MainFailure.ClientFailure());
     }
   }
-  
 }
