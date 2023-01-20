5e7ec1b2105e1450063679b28841dd78987a951b
Anil Antony <anilvithayathil007@gmail.com>
2023-01-20 14:52:26 +0530

    dio error solved

--- netflix/lib/Application/fast_Laugh/fastlaugh_bloc.dart
+++ netflix/lib/Application/fast_Laugh/fastlaugh_bloc.dart
@@ -13,8 +13,9 @@ final dummyVideoUrls = [
   "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
   "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
   "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
-  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+  "https://www.youtube.com/watch?v=-6xXuSmBox4",
 ];
 ValueNotifier<Set<int>> likedVideosIdsNotifier = ValueNotifier({});
 ValueNotifier<Set<int>> pauseVideosIdsNotifier = ValueNotifier({});
